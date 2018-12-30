67b5db1acf44e1321f08ce753287e12bbbf1b774
Yeregorix <yeregorixcraft@gmail.com>
2018-12-30 22:32:50 +0100

    Convert to Gradle

---
+++ src/main/java/net/smoofyuniverse/maze/UserInterface.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2017 Hugo Dupanloup (Yeregorix)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.smoofyuniverse.maze;
+
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import net.smoofyuniverse.common.app.App;
+import net.smoofyuniverse.common.fxui.dialog.Popup;
+import net.smoofyuniverse.common.fxui.field.IntegerField;
+import net.smoofyuniverse.common.fxui.field.LongField;
+import net.smoofyuniverse.common.fxui.task.Chrono;
+import net.smoofyuniverse.common.fxui.task.ObservableTask;
+import net.smoofyuniverse.common.task.Task;
+import net.smoofyuniverse.common.util.GridUtil;
+import net.smoofyuniverse.logger.core.Logger;
+import net.smoofyuniverse.maze.gen.Maze;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.function.Consumer;
+
+public final class UserInterface extends GridPane {
+	private static final Logger logger = App.getLogger("UserInterface");
+	private static final Random random = new Random();
+	
+	private static int taskId = 0;
+	
+	public UserInterface() {
+		Label title = new Label(App.get().getName());
+		title.setFont(Font.font("Monospaced", FontWeight.BOLD, 24));
+		
+		LongField seed = new LongField(0);
+		seed.setPrefWidth(Double.MAX_VALUE);
+		
+		IntegerField width = new IntegerField(1, 5000, 30), height = new IntegerField(1, 5000, 30);
+		IntegerField whitePx = new IntegerField(1, 100, 2), blackPx = new IntegerField(1, 100, 1);
+		
+		Button randomSeed = new Button("Aléatoire");
+		Button genMaze = new Button("Générer");
+		randomSeed.setPrefWidth(100);
+		genMaze.setPrefWidth(100);
+		
+		add(new Label("Graine: "), 0, 0);
+		add(seed, 1, 0, 3, 1);
+		add(randomSeed, 4, 0);
+		
+		add(new Label("Dimensions: "), 0, 1);
+		add(width, 1, 1);
+		add(new Label("x"), 2, 1);
+		add(height, 3, 1);
+		
+		add(new Label("Pixels: "), 0, 2);
+		add(whitePx, 1, 2);
+		add(new Label(":"), 2, 2);
+		add(blackPx, 3, 2);
+		add(genMaze, 4, 2);
+		
+		setAlignment(Pos.CENTER);
+		setPadding(new Insets(10));
+		setVgap(8);
+		setHgap(5);
+		getColumnConstraints().addAll(GridUtil.createColumn(), GridUtil.createColumn(Priority.ALWAYS), GridUtil.createColumn(), GridUtil.createColumn(Priority.ALWAYS), GridUtil.createColumn());
+		
+		FileChooser chooser = new FileChooser();
+		chooser.getExtensionFilters().add(new ExtensionFilter("Image", "*.png"));
+		
+		randomSeed.setOnAction((e) -> seed.valueProperty().set(random.nextLong()));
+		
+		genMaze.setOnAction((ev) -> {
+			File f = chooser.showSaveDialog(MazeGen.get().getStage().orElse(null));
+			if (f == null)
+				return;
+			
+			int widthV = width.getValue(), heightV = height.getValue();
+			int whitePxV = whitePx.getValue(), blackPxV = blackPx.getValue();
+			long seedV = seed.getValue();
+			int id = ++taskId;
+			
+			logger.info("Starting generation task #" + id + " .. (" + widthV + "x" + heightV + ", " + whitePxV + ":" + blackPxV + ", seed:" + seedV + ")");
+			
+			Chrono chrono = new Chrono(5);
+			ObservableTask t = new ObservableTask();
+			t.titleProperty().bind(Bindings.concat("Durée: ", chrono.textProperty()));
+
+			Consumer<Task> consumer = (task) -> {
+				chrono.start();
+
+				task.setMessage("Initialisation: " + widthV + "x" + heightV);
+				Maze maze = new Maze(widthV, heightV);
+				maze.task = task;
+				task.setMessage("Graine: " + seedV);
+				Random r = new Random(seedV);
+				
+				task.setMessage("Instanciation: " + maze.points.length + " points.");
+				maze.fill();
+				task.setMessage("Connection des points ..");
+				maze.connectAll(r);
+				
+				if (task.isCancelled()) {
+					chrono.pause();
+					System.gc();
+					
+					logger.info("Task #" + id + " has been cancelled. Duration: " + chrono.getText());
+				} else {
+					task.setMessage("Génération de l'image: " + whitePxV + ":" + blackPxV);
+					BufferedImage img = maze.createImage(whitePxV, blackPxV);
+					
+					chrono.pause();
+					System.gc();
+					
+					try {
+						task.setMessage("Écriture ..");
+						ImageIO.write(img, "png", f);
+						
+						logger.info("Task #" + id + " has terminated. Duration: " + chrono.getText());
+						
+						Popup.info().message("Graine: " + seedV + "\nDimensions: " + widthV + "x" + heightV + "\nPixels: " + whitePxV + ":" + blackPxV + "\nDurée: " + chrono.getText())
+						.title("Opération terminée").header("L'image labyrinthe a été générée et écrite avec succès.").show();
+					} catch (IOException e) {
+						logger.error("Task #" + id + " has terminated but failed to write the generated image. Duration: " + chrono.getText(), e);
+						
+						Popup.error().title("Erreur d'écriture").header("Une erreur est survenue lors de l'écriture de l'image.").message(e).show();
+					}
+				}
+			};
+			
+			Popup.consumer(consumer).task(t).title("Génération ..").submitAndWait();
+		});
+	}
+}
---
+++ src/main/java/net/smoofyuniverse/maze/gen/Maze.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2017 Hugo Dupanloup (Yeregorix)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.smoofyuniverse.maze.gen;
+
+import net.smoofyuniverse.common.task.Task;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class Maze {
+	public final int width, height;
+	public final Point[] points;
+
+	public Task task;
+	
+	private double lastUpdate;
+	
+	public Maze(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.points = new Point[width * height];
+	}
+	
+	private void update(double progress) {
+		if (progress - this.lastUpdate > 0.001)
+			forceUpdate(progress);
+	}
+	
+	private void forceUpdate(double progress) {
+		this.lastUpdate = progress;
+		if (this.task != null)
+			this.task.setProgress(progress);
+	}
+	
+	public boolean contains(int x, int y) {
+		return x >= 0 && x < this.width && y >= 0 && y < this.height;
+	}
+	
+	public Point get(int x, int y) {
+		return contains(x, y) ? this.points[y * this.width + x] : null;
+	}
+	
+	public void fill() {
+		forceUpdate(0);
+		
+		int pos = 0;
+		for (int y = 0; y < this.height; y++) {
+			for (int x = 0; x < this.width; x++) {
+				this.points[pos] = new Point(x, y);
+				pos++;
+				
+				update(pos / (double) this.points.length);
+			}
+		}
+		
+		forceUpdate(1);
+	}
+	
+	public boolean allConnected() {
+		return this.points[0].group.size() == this.points.length;
+	}
+	
+	public void connectAll(Random r) {
+		if (this.task != null)
+			this.task.setCancelled(false);
+		
+		forceUpdate(0);
+		
+		// Shuffle all
+		int it = 0;
+		for (Point p : this.points) {
+			p.shuffleCombinations(r);
+
+			it++;
+			update(it / (double) this.points.length);
+		}
+		
+		forceUpdate(1);
+		
+		// Prepare to watch progress
+		int phase1 = this.points.length;
+		int phase2 = (int) (phase1 * 0.25);
+		
+		double p1 = (1d / (double) phase1) * 0.1;
+		double p2 = (1d / (double) phase2) * 0.9;
+		
+		double progress = 0;
+		it = 0;
+		forceUpdate(0);
+		
+		// Connect all
+		RandomQueue<Point> queue = RandomQueue.of(this.points, r);
+		while (!allConnected() && (this.task == null || !this.task.isCancelled())) {
+			queue.next().connect();
+			
+			// Watch progress
+			it++;
+			if (it < phase1)
+				progress += p1;
+			else
+				progress += p2;
+			update(progress);
+		}
+		
+		forceUpdate(1);
+	}
+
+	public BufferedImage createImage(int whitePx, int blackPx) {
+		forceUpdate(0);
+
+		int imgWidth = (this.width * whitePx) + ((this.width + 1) * blackPx);
+		int imgHeight = (this.height * whitePx) + ((this.height + 1) * blackPx);
+		BufferedImage img = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_BYTE_BINARY);
+
+		Graphics2D g = img.createGraphics();
+		g.setColor(Color.WHITE);
+
+		int pos = 0;
+		int imgY = blackPx;
+		for (int y = 0; y < this.height; y++) {
+			int imgX = blackPx;
+			for (int x = 0; x < this.width; x++) {
+				Point p = this.points[pos++];
+
+				g.fillRect(imgX, imgY, whitePx, whitePx);
+				if (p.connectedRight)
+					g.fillRect(imgX + whitePx, imgY, blackPx, whitePx);
+				if (p.connectedDown)
+					g.fillRect(imgX, imgY + whitePx, whitePx, blackPx);
+
+				update(pos / (double) this.points.length);
+				imgX += whitePx + blackPx;
+			}
+			imgY += whitePx + blackPx;
+		}
+
+		forceUpdate(1);
+
+		return img;
+	}
+	
+	public class Point extends Group.Member {
+		public final int posX, posY;
+
+		private boolean connectedRight = false, connectedDown = false;
+		private Direction[] directions;
+		private int dirIndex;
+
+		public Point(int posX, int posY) {
+			this.posX = posX;
+			this.posY = posY;
+		}
+
+		public void shuffleCombinations(Random r) {
+			this.directions = Direction.randomCombination(r);
+			this.dirIndex = -1;
+		}
+
+		public void connect() {
+			while (this.dirIndex < 3) {
+				this.dirIndex++;
+				if (connect(this.directions[this.dirIndex]))
+					return;
+			}
+		}
+
+		public Point getRelative(Direction d) {
+			return get(this.posX + d.dX, this.posY + d.dY);
+		}
+
+		public boolean connect(Direction d) {
+			Point p2 = getRelative(d);
+			if (!this.group.append(p2))
+				return false;
+
+			switch (d) {
+			case UP:
+				p2.connectedDown = true;
+				break;
+			case DOWN:
+				this.connectedDown = true;
+				break;
+			case LEFT:
+				p2.connectedRight = true;
+				break;
+			case RIGHT:
+				this.connectedRight = true;
+				break;
+			}
+			return true;
+		}
+	}
+}
